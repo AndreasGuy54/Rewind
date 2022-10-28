7171aafe2d62250fdbc7ea29ba88cf77364dde0e
Andreas Abaidoo <abaidooandy@gmail.com>
2022-10-28 14:38:36 +0000

    Completed js functions and code page

--- js/site.js
+++ js/site.js
@@ -1,50 +1,46 @@
 // Get the string from the input on the app page (controller function)
 const getValue = () => {
 
+    document.querySelector("#alert").classList.add("invisible");
+
+    let userInput = document.querySelector("#userInput").value;
+    let reversedInput = reverseString(userInput);
+
+    displayString(reversedInput);
 }
 
 // Reverse the string (logic function)
-const reverseString = () => {
+const reverseString = (input) => {
+    let revString = [];
+    // let revString = ""
 
+    for (let i = (input.length - 1); i >= 0; i--) {
+        revString.push(input[i]);
+        //revString += userString[i];
+    }
+
+    return revString.join("");
 }
 
 // Display the reversed string to the user (view function)
-const displayString = () => {
-
-}
-
-
-const getValues = () => {
-    let firstNum = parseInt(document.querySelector('#startValue').value)
-    let lastNum = parseInt(document.querySelector('#endValue').value)
-
-    let numbers = generateNumbers(firstNum, lastNum);
-
-    displayNumbers(numbers);
-}
-// generate numbers from the start value to the end value
-const generateNumbers = (startVal, endVal) => {
-    let nums = [];
-
-    for (let i = startVal; i <= endVal; i++) {
-        nums.push(i);
-    }
-    return nums;
+const displayString = (reversed) => {
+    document.querySelector("#message").innerHTML = `Your string when reserved is: ${reversed}`;
+    document.querySelector("#alert").classList.remove("invisible");
 }
 
 // Display the numbers and make even numbers bold
-const displayNumbers = (nums) => {
-    let templateRows = '';
+// const displayNumbers = (nums) => {
+//     let templateRows = '';
 
-    nums.forEach(num => {
-        let className = '';
+//     nums.forEach(num => {
+//         let className = '';
 
-        if (num % 2 === 0) {
-            className = 'even';
-        } 
+//         if (num % 2 === 0) {
+//             className = 'even';
+//         } 
         
-        templateRows += `<tr><td class="${className}">${num}</td></tr>`;
-    });
+//         templateRows += `<tr><td class="${className}">${num}</td></tr>`;
+//     });
 
-    document.querySelector('#results').innerHTML = templateRows;
-}
+//     document.querySelector('#results').innerHTML = templateRows;
+// }
